94c27a972051f319be85a401c5125c1faa68d151
llucasmota <lucas.o.mota@gmail.com>
2018-05-08 23:10:02 -0300

    Tarefa Udacity
    Substituição de strings

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,14 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+
+ var awesomeThoughts = "My name is Cameron and I am AWESOME!";
+ var funThoughts = awesomeThoughts.replace("AWESOME!", "FUN!");
+
+ $("#main").append(funThoughts);
+
+ var formatteName = HTMLheaderName.replace("%data%", "Lucas Mota");
+ $("#header").append(formatteName);
+
+ var formatteRole = HTMLheaderRole.replace("%data%", "Front-End Developer");
+ $("#header").append(formatteRole);

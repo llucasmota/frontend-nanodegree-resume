8222964010787528192e8eb954ae88e519089454
llucasmota <lucas.o.mota@gmail.com>
2018-05-16 23:17:21 -0300

    Adicionando Json Work e Schools

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -3,18 +3,18 @@ This is empty on purpose! Your code to build the resume will go here.
  */
 
 var bio = {
-    "name": "Lucas Mota",
-    "role": "Front End Developer",
-    "contact": {
-        "mobile": "61982561940",
-        "email": "lucas.o.mota@gmail.com",
-        "github": "https://github.com/llucasmota/Portifolio-Project",
-        "twitter": "twitter.com/llucasmota",
-        "location": "Brasília - Brazil"
-    },
-    "welcomeMessage": "Seja bem-vindo ao meu portifólio",
-    "skills": ["Html5", "Jquery", "Javascript", "End-To-End Test"],
-    "biopic": "images/fry.jpg"
+  "name": "Lucas Mota",
+  "role": "Front End Developer",
+  "contact": {
+    "mobile": "61982561940",
+    "email": "lucas.o.mota@gmail.com",
+    "github": "https://github.com/llucasmota/Portifolio-Project",
+    "twitter": "twitter.com/llucasmota",
+    "location": "Brasília - Brazil"
+  },
+  "welcomeMessage": "Seja bem-vindo ao meu portifólio",
+  "skills": ["Html5", "Jquery", "Javascript", "End-To-End Test"],
+  "biopic": "images/fry.jpg"
 }
 
 var formatteName = HTMLheaderName.replace("%data%", bio.name);
@@ -22,3 +22,72 @@ $("#header").prepend(formatteName);
 var formatteRole = HTMLheaderRole.replace("%data%", bio.role);
 $("#header").prepend(formatteRole);
 
+/*
+jobs: array of objects with
+         employer: string 
+         title: string 
+         location: string 
+         dates: string (Can be 'in progress')
+         description: string 
+
+*/
+var work = [
+  {
+    "job": {
+      "employer": "Mirante Tecnologia",
+      "title": "Analista de Teste",
+      "location": "Brasília",
+      "dates": "Junho de 2017 até o momento.",
+      "description": "Testes Funcionais, Automatizados e etc."
+
+    },
+    "job": {
+      "employer": "CTIS",
+      "title": "Testador de Software",
+      "location": "Brasília",
+      "dates": "Agosto de 2015 até junho de 2017.",
+      "description": "Testes Funcionais, Automatizados e etc."
+
+    },
+    "job": {
+      "employer": "Tecnisys",
+      "title": "Técnico de Suporte",
+      "location": "Brasília",
+      "dates": "Fevereiro de 2014 até julho de 2015",
+      "description": "TerereuTerereu"
+
+    }
+  }
+]
+
+$("#main").append(work[0].job.title);
+
+//   education contains:
+
+//   schools: array of objects with
+//        name: string
+//        location: string
+//        degree: string
+//        majors: array of strings
+//        dates: string (works with a hyphen between them)
+//        url: string (optional)
+//   onlineCourses: array of objects with
+//        title: string
+//        school: string
+//        dates: string (works with a hyphen between them)
+//        url: string
+
+var education = [
+  {
+    "schools": {
+      "name": "Análise e Desenvolvimento de Sistemas",
+      "location": "Brasília",
+      "degree": "Superior",
+      "majors": ["alguma coisa", "duas coisas"],
+      "dates": "2014 - 2017",
+      "url":  "github.com/llucasmota" 
+    }
+  }
+]
+
+$('#main').append(education[0].schools.name)
